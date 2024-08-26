032338ef6d8ed795264f570db8b438488c2f9c73
Kevin Dang <kevin@edgered.com.au>
2024-08-26 05:44:07 +0000

    reference to source table

---
+++ definitions/sources/sources.js
@@ -0,0 +1,23 @@
+const sourceConfigs = [
+    {
+        database: "edgered-lab",
+        schemas: [{
+            schema: "demo_bronze",
+            tables: [
+                "payments"
+            ]
+        }]
+    }
+];
+
+sourceConfigs.forEach(({ database, schemas }) => {
+    schemas.forEach(({ schema, tables }) => {
+        tables.forEach(name => {
+            declare({
+                database,
+                schema,
+                name
+            });
+        });
+    });
+});
