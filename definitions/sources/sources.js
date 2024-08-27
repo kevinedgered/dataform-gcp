631d6c100486cef7acd9bdbb69a918571cc23558
Kevin Dang <kevin@edgered.com.au>
2024-08-27 23:33:32 +0000

    dummy commit

--- definitions/sources/sources.js
+++ definitions/sources/sources.js
@@ -6,8 +6,8 @@ const sourceConfigs = [
             tables: [
                 "payments",
                 "clients"
-            ]
-            },
+             ]
+            }
 
         ]
     }
